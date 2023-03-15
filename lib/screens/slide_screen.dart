46377ed061c0863162bd6614adb58344d45776fa
gabriel12QK <jogar737@gmail.com>
2023-03-15 17:26:11 -0500

    checkbox y siwch

--- lib/screens/slide_screen.dart
+++ lib/screens/slide_screen.dart
@@ -9,34 +9,55 @@ class slideScreen extends StatefulWidget {
 
 class _slideScreenState extends State<slideScreen> {
   double _slideValue = 100;
+  bool _isValid = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Inputs y Forms'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-                min: 50,
-                max: 600,
-                value: _slideValue,
-                onChanged: (value) {
-                  _slideValue = value;
-                  setState(() {});
-                }),
-            Image(
-              image: NetworkImage(
-                  'https://w7.pngwing.com/pngs/420/615/png-transparent-marvel-spider-man-illustration-ultimate-spider-man-hulk-standee-poster-spider-comics-heroes-superhero.png'),
-              fit: BoxFit.contain,
-              width: _slideValue,
+      body: Column(
+        children: [
+          Slider.adaptive(
+              min: 50,
+              max: 600,
+              value: _slideValue,
+              onChanged: _isValid
+                  ? (value) {
+                      _slideValue = value;
+                      setState(() {});
+                    }
+                  : null),
+          //solo representa el cuadrado a darle click
+          // Checkbox(
+          //     value: _isValid,
+          //     onChanged: (value) {
+          //       _isValid = value ?? true;
+          //       setState(() {});
+          //     }),
+          // CheckboxListTile(
+          //     title: Text('Habilitar slider'),
+          //     value: _isValid,
+          //     onChanged: ((value) => setState(() {
+          //           _isValid = value ?? true;
+          //         }))),
+          SwitchListTile.adaptive(
+              title: Text('Habilitar slider'),
+              value: _isValid,
+              onChanged: ((value) => setState(() {
+                    _isValid = value ?? true;
+                  }))),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: NetworkImage(
+                    'https://w7.pngwing.com/pngs/420/615/png-transparent-marvel-spider-man-illustration-ultimate-spider-man-hulk-standee-poster-spider-comics-heroes-superhero.png'),
+                fit: BoxFit.contain,
+                width: _slideValue,
+              ),
             ),
-            const SizedBox(
-              height: 50,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
